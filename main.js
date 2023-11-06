37622e860121e9bf7117ff63a94d28acdc468e33
gabrielladenega <127108435+gabrielladenega@users.noreply.github.com>
2023-11-06 09:35:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function tocaSomLobo(){
-    document.querySelector("#som_tecla_lobo").play();
+function tocaSomVaca(){
+    document.querySelector("#som_tecla_vaca").play();
 }
 const listadeTeclas = document.querySelectorAll(".tecla");
 
-listadeTeclas[2].onclick = tocaSomLobo;
+let contador = 0;
+while(contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomAplausos;
+    contador = contador + 1;
+    console.log(contador);
+}
